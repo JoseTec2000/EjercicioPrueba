d10b49b165adc7599bdbbd1d407b633ae7d88df7
JoseTec2000 <46898182+JoseTec2000@users.noreply.github.com>
2019-01-21 09:40:19 -0600

    José Isidro Sánchez Vázquez, Grupo 02.

---
+++ VelocidadPromedio.py
@@ -0,0 +1,7 @@
+#Autor:José Isidro Sánchez Vázquez
+#Programa que calcula la velocidad promedio en un viaje
+
+d=float(input("Dame la distancia recorrida en kilometros: "))
+t=float(input("Dame el tiempo que tardo en llegar de una ciudad a otra en horas: "))
+v=d/t
+print("La velocidad promedio del vehiculo a lo largo del viaje fue: ",v,"km/h")
